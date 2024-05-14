24ab717d321845cbabb363a2e3cbb6a41082ea57
Willian Alves <wba.alves@gmail.com>
2024-05-14 01:03:56 -0300

    add bright thing to make it fun

--- src/components/Picture/Picture.ts
+++ src/components/Picture/Picture.ts
@@ -4,10 +4,16 @@ import "./Picture.css";
 export const Picture = () => {
   return {
     view: () => {
-      return m("img", {
-        class: "picture",
-        src: "https://github.com/willianba.png",
-      });
+      return m(
+        ".box",
+        m(".card", [
+          m("img", {
+            class: "picture",
+            src: "https://github.com/willianba.png",
+          }),
+          m(".blob"),
+        ]),
+      );
     },
   };
 };
